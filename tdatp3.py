5a5471da6716137bcb20f8598dab6cd7f7dffb9e
Alejandro Pernin <ale.pernin@gmail.com>
2013-11-19 04:33:27 -0300

    Definidas estructuras y aproximador

--- tdatp3.py
+++ tdatp3.py
@@ -2,13 +2,50 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from types import *
 
-class objeto():
+class Objeto():
 	def __init__(self, tam):
+		assert type(tam) is FloatType
 		self.tam = tam
 
-	def __str__():
+	def __str__(self):
+		return str(self.tam)
+
+	def getTam(self):
 		return self.tam
+
+class Envase():
+	def __init__(self):
+		self._list=[]
+		self.max=1
+		self.count=0
+
+	def __str__(self):
+		s = "["
+		for i in self._list:
+			s += "%s, " % i
+		s += "]"
+		return s
+
+	def envasar(self, obj):
+		if (self.count+obj.getTam())<=self.max:
+			self._list.append(obj)
+			self.count+=obj.getTam()
+			return True
+		else:
+			return False
+
+def aprox(l):
+	r=[]
+	e=Envase()
+	for obj in l:
+		if not e.envasar(obj):
+			r.append(e)
+			e=Envase() #Se abre otro envase
+			e.envasar(obj)
+	r.append(e)
+	return r
 
 def loadFile():
 	try:
@@ -16,6 +53,17 @@ def loadFile():
 	except IOError:
 		print "El archivo no pudo ser abierto, verificar parametros"
 		usage()
+		return False
+	l=[]
+	n=_file.readline().replace('\n','')
+	n=int(n)
+	_file.readline() #Linea en blanco
+	for line in _file:
+		tam=line.replace('\n','').replace(',','.')
+		tam=float(tam)
+		obj=Objeto(tam)
+		l.append(obj)
+	return l
 
 def usage():
 	print "Usage:"
@@ -30,8 +78,14 @@ def checkArgs():
 	return True
 
 def main():
-	if not checkArgs() or not loadFile():
+	l=None
+	if not checkArgs():
 		return
-
+	l=loadFile()
+	if not l:
+		return
+	r = aprox(l)
+	for l in r:
+		print l
 if __name__ == '__main__':
 	main()
